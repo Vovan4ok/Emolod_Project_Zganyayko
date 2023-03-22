b0f4541f8d333b515f1f471600917b5164e6407e
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-22 13:11:24 +0300

    added project to the repository

---
+++ Project_Zganyayko_Volodymyr/Fireball.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class Fireball : Skill
+    {
+        public Fireball()
+        {
+            this.name = "Fireball";
+        }
+        public override void useSkill(Monster monster)
+        {
+            monster.setHealth(monster.getHealth() - 500);
+        }
+    }
+}
